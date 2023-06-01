5efed0ebc8d02cb423b1e2d91f90b3040612febc
ltlaitoff <ltlaitoff@gmail.com>
2023-06-01 21:11:07 +0300

    feat(error-interceptor): Created stub for `new_json` http requests

--- cheapTrip/src/app/error-interceptor.ts
+++ cheapTrip/src/app/error-interceptor.ts
@@ -52,6 +52,17 @@ export class ErrorInterceptor implements HttpInterceptor {
         let errorData: AlertMessage;
         console.log('--==unknown error==-- ' + JSON.stringify(error));
 
+        if (error.url.includes('new_json')) {
+          console.log(
+            '%c[ErrorInterceptor]:',
+            'color: red;',
+            'Ignoring all `new_json` error',
+            error
+          );
+
+          return throwError(error);
+        }
+
         switch (true) {
           case error.status === 500:
             errorData = new AlertMessage(
